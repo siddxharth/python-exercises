0f1dd7f156637ff53a1fc7ddbff4bc5a18693d19
siddxharth <siddxharth@gmail.com>
2020-08-06 15:53:37 +0530

    Ex16: Added password level(Easy/Medium/Hard)
    If choice is Easy, password only contains Letters.\nIf choice is Medium, password contains Letters and Numbers.\n If choice is Hard, password contains Letters, Numbers and Special Characters.

--- Exercise16.py
+++ Exercise16.py
@@ -1,5 +1,7 @@
 import random
 
+choice = input("Enter type of password(\"Easy/Medium/Hard)")
+choice = choice.lower()
 password = int(input("Enter length of password: "))
 numbers = ['1','2','3','4','5','6','7','8','9','0']
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -9,17 +11,35 @@ character = ''
 generated = ''
 flag = 1
 
-while flag<=password:
-    rand = random.randint(1,3)
-    if rand == 1:
-        character = numbers
-    elif rand == 2:
-        character = letters
-    else:
-        character = special        
-    
-    element = random.choice(character)
-    generated += element
-    flag+=1
-
+if choice == "h":
+    while flag<=password:
+        rand = random.randint(1,3)
+        if rand == 1:
+            character = numbers
+        elif rand == 2:
+            character = letters
+        else:
+            character = special      
+        
+        element = random.choice(character)
+        generated += element
+        flag+=1
+elif choice == "m":
+    while flag<=password:
+        rand = random.randint(1,2)
+        if rand == 1:
+            character = numbers
+        else:
+            character = letters 
+        element = random.choice(character)
+        generated += element
+        flag+=1
+elif choice == "e":
+    while flag<=password:
+        character = letters 
+        element = random.choice(character)
+        generated += element
+        flag+=1
+else:
+    print("Invalid password choice.")
 print(generated)
